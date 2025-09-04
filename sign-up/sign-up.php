40d20fc79449924c8bf0e9391dc1036b1201d9ca
trongtuan-io <tuannguyentrong1202@gmail.com>
2025-09-04 22:26:05 +0700

    html css sign-up

---
+++ sign-up/sign-up.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="sign-up.css">
+</head>
+<body>
+    <main class="auth">
+        <div class="auth_content">
+            <div class="auth_content_inner">
+                <h1>Sign up</h1>
+                <div class="auth_desc"></div>
+                <form action="" class="auth_form">
+                    <div class="form_group">
+                        <div class="form_text_input">
+                            <input type="email" name="" id="" placeholder = "Email" class="form_input">
+                            <img src="" alt="" class="form_input_icon">
+                        </div>
+                    </div>
+
+                       <div class="form_group">
+                        <div class="form_text_input">
+                            <input type="password" name="" id="" placeholder = "Password" class="form_input">
+                            <img src="" alt="" class="form_input_icon">
+                        </div>
+                    </div>
+
+                       <div class="form_group">
+                        <div class="form_text_input">
+                            <input type="password" name="" id="" placeholder = "Confirm password" class="form_input">
+                            <img src="" alt="" class="form_input_icon">
+                        </div>
+                    </div>
+                     <div class="form_group">
+                        <a href="" class="forgot_password">Forgot your password?</a>
+                     </div>
+                     <div class="form_group auth_btn-group">
+                        <button class="btn_sign_up">Sign up</button>
+                     </div>
+                </form>
+
+                <p class="auth_text">
+                    Bạn đã có tài khoản?
+                    <a href="../login/login.php">Login</a>
+                </p>
+
+            </div>
+        </div>
+    </main>
+</body>
+</html>
