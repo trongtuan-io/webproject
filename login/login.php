3b1c420e95a71604ad684a2a54a3b182760194ed
trongtuan-io <tuannguyentrong1202@gmail.com>
2025-09-04 22:24:01 +0700

    first commit

---
+++ login/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="/login/login.css?v=2">
+</head>
+<body>
+  <main class="auth">
+    <div class="auth_content">
+      <div class="auth_content_inner">
+        <h1>Login</h1>
+
+        <form class="auth_form">
+          <input type="email" placeholder="Email" class="form_input">
+          <input type="password" placeholder="Password" class="form_input">
+          <button type="submit" class="btn_sign_up">Login</button>
+        </form>
+
+        <p class="auth_text">
+          Chưa có tài khoản?
+          <a href="../sign-up/sign-up.php">Sign up</a>
+        </p>
+      </div>
+    </div>
+  </main>
+</body>
+</html>
